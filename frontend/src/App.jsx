39cd1f5f86e8bd3cf23408bcf8e0c60036f01194
M-talha-mumtaz <mtalha3632@gmail.com>
2026-04-26 15:49:16 +0500

    Add ScrollToTop component to fix navigation scroll position

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout';
 import AnimatedRoutes from './components/AnimatedRoutes';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <AnimatedRoutes />
       </Layout>
